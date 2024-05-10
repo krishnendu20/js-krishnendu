884ca091f4c9ff4fdb6da1078a70c0ddd922b66d
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-05-10 10:17:08 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 16533
+let accountEmail = "Krishnendu.dip.aiub@gmail.com"
+var accountPassword = "921245"
+accountCity = "dhaka"
+let accountState;
+
+// accountId = 2
+accountEmail="kd@kg.com"
+accountPassword="543543"
+accountCity="Bogura"
+console.log(accountId);
+console.table ([accountEmail, accountPassword,accountId, accountCity,accountState])
