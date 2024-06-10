332ce6c0cb019e051e6981c3ebea9b79c6bda78a
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-10 09:13:30 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "love"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+
+// console.log(valueInNumber);
+
+//"33"=33
+//"33abc"=Nan
+//true=>1; false=0
+//1=true 0=false
+//""=false
+//"love"= true
+
+let isLoggedIn = 0
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+let someNumber = 34
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
