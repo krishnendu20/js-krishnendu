abe2a8bcb0a7657d12bc7a3749f7a3d234faa049
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-10 07:47:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all Js code as newar version
+
+// a;ert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3); // code readability should be high
+    
+    console.log("Krishnendu");
+
+    let name = "Dibbyo"
+    let age = 19
+    let isLoggedin = false
+    let state;
+
+    //number=> 2 to powr 53
+    //bigint =>
+        //string=> ""
+    //boolean= true/false
+    //null= standard value
+//undefined=>
+    // symbol=> unique
+
+
+//object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
