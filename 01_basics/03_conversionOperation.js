9151589c20b44c36786ec897369c6c4b6ee4c646
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-11 07:33:48 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,3 +24,34 @@ let someNumber = 34
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+// link to study:https://tc39.es/ecma262/#sec-toprimitive
+// let str1 = "Hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1+"2");
+// console.log("1" +2 + 2);
+// console.log((1+2)+"2"); //use () if string in fast all will treat to string and if string in last than do fast operation
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3= 2+2
+
+let gameCounter =100 //link to study: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+gameCounter++;
+console.log(gameCounter);
