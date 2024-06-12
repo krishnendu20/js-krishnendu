f1def95f6c20521adfe877bbe2c02ab0c2e48373
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-12 04:34:47 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// primitive
+
+// 7 types: String ,Number,Boolean,null,undefined, symbol,Bigint
+
+const score = 100
+const scoreValue = 100.2
+
+const isLoggedin = false
+
+const outsideTemp = null
+let userEmail;
+const id = Symbol('456')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 26326252525n //linl to study:https://tc39.es/ecma262/#sec-ecmascript-language-source-code
+// Reference(Not primitive)
+
+// Array, objects, function
+
+const heros = ["saktiman","Batman","spiderman","aquaman","superman"];
+
+let myobj = {
+    name: "Krishnendu",
+    age: 22,
+
+}
+
+const myFunction = function()
+{
+    console.log("Hello Developers");
+  
+}
+console.log (typeof outsideTemp);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof  id);
