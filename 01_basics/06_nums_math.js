e11cfd516cab79de286059ec8eca341475b70922
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-13 08:08:51 +0000

    math and num

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 34.3323
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//*************MATHS&&&&&&& */
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(6.5));
+console.log(Math.floor(6.9));
+console.log(Math.min(3,2,4,6));
+console.log(Math.max(5,7,2,9));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
