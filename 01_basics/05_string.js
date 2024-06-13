b0f6ecb251dd54bb3eeff3447bad29ae4dcd350c
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-13 05:01:17 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name = "Dibbyo"
+const repoCount= 49
+const mylove = "Tithi Roy"
+
+// console.log( name + repoCount + " Value");
+
+console.log(`My name is ${name} and i have git repo ${repoCount} and my love is ${mylove}`);
