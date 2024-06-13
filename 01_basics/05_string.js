9b02d0c58a31d112d23b71856a8e7e343a005e51
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-13 06:45:39 +0000

    all about string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,3 +5,30 @@ const mylove = "Tithi Roy"
 // console.log( name + repoCount + " Value");
 
 console.log(`My name is ${name} and i have git repo ${repoCount} and my love is ${mylove}`);
+
+const gameName = new String("NILadrOT-LO-ve-tithi")
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt("5"));
+console.log(gameName.indexOf('r'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,8)
+console.log(anotherString);
+
+const newString1 = "    poloTshirt   "
+console.log(newString1);
+console.log(newString1.trim());
+
+const url ="https://krishnendu.com/krishnendu%20das"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('das'))
+
+console.log(gameName.split('-')); // link to study:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+ 
