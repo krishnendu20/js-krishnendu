fc41b513ed42756cc4d069b45c04882ebefbbca2
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-16 05:25:22 +0000

    dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,12 @@
+//dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myNewdate = new Date(2023, 0, 23)
+
+let myNewdate = new Date("2024-03-22")
+console.log(myNewdate.toLocaleString());
