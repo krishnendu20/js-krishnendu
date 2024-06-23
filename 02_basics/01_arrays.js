b09fefe9733a27601e6d7f3cd6e046b4bfe615b0
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-23 04:45:48 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+//arrays
+
+const myArr = [0,1,2,3,4,5]
+const myheros = ["allu","dhanush","srk"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[2]);
+
+//array methods
+
+// myArr2.push(5)
+// // myArr2.pop
+// myArr2.unshift(9)
+// myArr2.shift()
+
+// console.log(myArr2.includes(9));
+// console.log(myArr2.indexOf(9));
+// const newArr = myArr2.join()
+// console.log(myArr2);
+// console.log( newArr);
+
+// slice , splice
+
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr);
+
+const myn2 =myArr.splice(1,3)
+console.log("c ",myArr);
+console.log(myn2);
+ 
+
