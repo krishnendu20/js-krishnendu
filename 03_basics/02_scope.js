0579fc97d22005b623a8443a9e352e3d66c6ea9a
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-06-26 05:09:31 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,47 @@
+
+var c = 444
+
+if (true) {
+    let a = 19
+const b =20
+let c = 33
+// console.log("INEER: ",c);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+    const username = "Krish"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+// one()
+
+if (true) {
+const username = "Dibbyo"
+if (username==="Dibbyo") {
+    const website = " youtube"
+    // console.log(username + website);
+}
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+// *********Interesting************
+console.log(addone(5));
+function addone(num){
+    return num +1
+}
+
+console.log(addTwo(5));
+const addTwo = function(num) {
+    return num + 2
+}
