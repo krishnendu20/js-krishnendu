28d111cd72c20fd2d55ad5fe9283dd9f12d5bd92
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-07-01 08:30:22 +0000

    while do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray=["flash","superman","batman"]
+let arr =0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr=arr+1
+}
+
+let run = 12
+
+do {
+    console.log(`Score is ${run}`);
+    run ++
+} while (run <=10);
+
