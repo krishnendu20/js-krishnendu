e69ae4685f67e286b3edeae3fff9aa8ad76625ef
Krishnendu Das <114773164+krishnendu20@users.noreply.github.com>
2024-07-02 19:26:01 +0000

    loop

---
+++ 05_iterations/five.js
@@ -0,0 +1,26 @@
+// const coding = ["js","ruby","swift","typescript","java"]
+
+// const value = coding.forEach(  (item)=>{
+//   console.log(item);
+  
+// })
+// console.log(value);
+//implicit return
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter ( (num) =>{
+//     return num > 4
+// })
+
+
+const newNum = []
+
+myNums.forEach( (num) =>{
+ if (num >4 ) {
+    
+ }
+})
+
+console.log(newNums);
+
